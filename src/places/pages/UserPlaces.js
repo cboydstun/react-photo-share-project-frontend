73f5fc8390340ee2eb4ae7a3605c81ec3c937d34
Chris Boydstun <chrisboydstun@gmail.com>
2019-12-02 17:39:02 -0600

    fixed picture bug

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -9,7 +9,7 @@ const DUMMY_PLACES =[
         title: 'Empire State Building',
         description: 'One of the tallest places in Manhattan.',
         address: '20 W 34th St, New York, NY',
-        imageURL: 'https://media.timeout.com/images/101705309/750/422/image.jpg',
+        imageUrl: 'https://media.timeout.com/images/101705309/750/422/image.jpg',
         location: {
             lat: 40.7484405,
             lon: -73.9878584
@@ -20,7 +20,7 @@ const DUMMY_PLACES =[
         id: 'p2',
         title: 'Empire State Building',
         description: 'One of the tallest places in Manhattan.',
-        imageURL: 'https://cdn.getyourguide.com/img/tour_img-1739965-148.jpg',
+        imageUrl: 'https://cdn.getyourguide.com/img/tour_img-1739965-148.jpg',
         address: '20 W 34th St, New York, NY',
         location: {
             lat: 40.7484405,
